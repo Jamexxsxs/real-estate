e0e1d9717796b33d42396dcc9e423c50c7c861f9
Jomary Coquilla <jomarycoquilla465@gmail.com>
2023-11-11 00:59:29 +0800

    .

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# Home
-@app.route('/')
+@app.route("/.netlify/functions/app", methods=["GET"])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# Properties
-@app.route('/properties')
+
+@app.route("/.netlify/functions/app/properties", methods=["GET"])
 def properties():
-    return render_template('properties-tab/property.html')
+    return render_template("properties-tab/property.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=5000)
